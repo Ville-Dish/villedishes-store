fbfdb62ca21aef2b0c2ac70f922bcb2d2889b186
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-11-08 12:48:24 -0700

    Refactor file for conditional paragraph display

--- emails/email-footer.tsx
+++ emails/email-footer.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import { Section, Text } from "@react-email/components";
 
-export const EmailFooter = () => {
+type Props = {
+  variant: "contact" | "order" | "payment";
+};
+
+export const EmailFooter = ({ variant }: Props) => {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
+
+  // Define the dynamic text based on the variant
+  const dynamicText = (() => {
+    switch (variant) {
+      case "contact":
+        return "You can reply this mail to directly respond to the customer.";
+      case "order":
+        return "Do not reply to this mail. If you have any concerns about the order, use our contact form on the website to log your complain or email us directly on villedishes@gmail.com";
+      case "payment":
+        return "Do not reply to this mail. If you have any concerns about the payment, check your admin dashboard with the order id and verification code to get customer details or contact admin.";
+      default:
+        return "This is an automated message. Please do not reply to this email.";
+    }
+  })();
 
   return (
     <Section
@@ -16,9 +34,7 @@ export const EmailFooter = () => {
       }}
     >
       <Text>&copy; {currentYear} Villedishes. All rights reserved.</Text>
-      <Text className="dynamic-text">
-        This is an automated message. Please do not reply to this email.
-      </Text>
+      <Text>{dynamicText}</Text>
       <Text>Email: villedishes@gmail.com | Phone: +1 (555) 789-4429</Text>
     </Section>
   );
