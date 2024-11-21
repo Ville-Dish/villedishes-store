a1c6869c4868332de4483cfd533f5e6dbfe98bbb
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-11-21 13:33:57 -0700

    Fix lint warning

--- app/admin/(dashboard)/dashboard/page.tsx
+++ app/admin/(dashboard)/dashboard/page.tsx
@@ -1,14 +1,6 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { DatePickerWithRange } from "@/components/custom/date-range-picker";
-import { Button } from "@/components/ui/button";
+"use client";
+
+import Link from "next/link";
 import {
   DollarSign,
   CreditCard,
@@ -18,18 +10,34 @@ import {
   ClockAlert,
   CircleX,
 } from "lucide-react";
-import { ReportsTable } from "@/components/custom/dashboard/report-table";
 import {
   adminOverViewData,
   adminProductPerformanceData,
   adminRevenueGrowthData,
   adminRecentOrders,
+  adminReportData,
 } from "@/lib/constantData";
 import { RevenueGrowth } from "@/components/custom/dashboard/revenue-growth";
 import { ProductPerformance } from "@/components/custom/dashboard/product-performance";
 import { RecentOrders } from "@/components/custom/dashboard/recent-orders";
 import { Overview } from "@/components/custom/dashboard/overview";
-import Link from "next/link";
+import { ReportsSection } from "@/components/custom/dashboard/report-accordion";
+import { DatePickerWithRange } from "@/components/custom/date-range-picker";
+import { YearPicker } from "@/components/custom/year-picker";
+import { MonthYearPicker } from "@/components/custom/month-year-picker";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+import { DateRange } from "react-day-picker";
+import { subDays } from "date-fns";
 
 const StatCard: React.FC<{
   title: string;
@@ -48,30 +56,116 @@ const StatCard: React.FC<{
 );
 
 export default function AdminDashboard() {
+  const [activeTab, setActiveTab] = useState("overview");
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 30),
+    to: new Date(),
+  });
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1);
+  const [filteredOverviewData, setFiltereredOverviewData] =
+    useState(adminOverViewData);
+  const [filteredRevenueData, setFilteredRevenueData] = useState(
+    adminRevenueGrowthData
+  );
+  const [filteredProductData, setFilteredProductData] = useState(
+    adminProductPerformanceData
+  );
+
+  const filterData = () => {
+    let fromDate: Date, toDate: Date;
+
+    if (activeTab === "overview" && dateRange?.from && dateRange?.to) {
+      fromDate = dateRange.from;
+      toDate = dateRange.to;
+    } else if (activeTab === "analytics") {
+      fromDate = new Date(selectedYear, 0, 1);
+      toDate = new Date(selectedYear, 11, 31);
+    } else if (activeTab === "reports") {
+      fromDate = new Date(selectedYear, selectedMonth - 1, 1);
+      toDate = new Date(selectedYear, selectedMonth, 0);
+    } else {
+      return; // If no valid date range, exit the function
+    }
+
+    // Filter Overview Data
+    const filteredOverview = adminOverViewData.filter((item) => {
+      const itemDate = new Date(`${item.name}-01`);
+      return itemDate >= fromDate && itemDate <= toDate;
+    });
+    setFiltereredOverviewData(filteredOverview);
+
+    // Filter Revenue Growth Data
+    const filteredRevenue = adminRevenueGrowthData.filter((item) => {
+      const itemDate = new Date(item.name);
+      return itemDate >= fromDate && itemDate <= toDate;
+    });
+    setFilteredRevenueData(filteredRevenue);
+
+    // Filter Product Performance Data
+    const filteredProduct = adminProductPerformanceData.filter((item) => {
+      const itemDate = new Date(item.date);
+      return itemDate >= fromDate && itemDate <= toDate;
+    });
+    setFilteredProductData(filteredProduct);
+  };
+
+  useEffect(() => {
+    filterData();
+  }, [activeTab, dateRange, selectedYear, selectedMonth, filterData]);
+
+  const renderDatePicker = () => {
+    switch (activeTab) {
+      case "overview":
+        return <DatePickerWithRange date={dateRange} setDate={setDateRange} />;
+      case "analytics":
+        return (
+          <YearPicker
+            selectedYear={selectedYear}
+            onYearChange={setSelectedYear}
+          />
+        );
+      case "reports":
+        return (
+          <MonthYearPicker
+            selectedMonth={selectedMonth}
+            selectedYear={selectedYear}
+            onMonthChange={setSelectedMonth}
+            onYearChange={setSelectedYear}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
-      <div className="flex items-center justify-between space-y-2">
+      <div className="flex flex-col sm:flex-row items-center justify-between space-y-2 sm:space-y-0">
         <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
         <div className="flex items-center space-x-2">
-          <DatePickerWithRange />
-          <Button>Filter</Button>
+          {renderDatePicker()}
+          <Button onClick={filterData}>Filter</Button>
         </div>
       </div>
-      <Tabs defaultValue="overview" className="space-y-4">
+      <Tabs
+        defaultValue="overview"
+        className="space-y-4"
+        onValueChange={setActiveTab}
+      >
         <TabsList className="w-full">
           <TabsTrigger value="overview">Overview</TabsTrigger>
           <TabsTrigger value="analytics">Analytics</TabsTrigger>
           <TabsTrigger value="reports">Reports</TabsTrigger>
         </TabsList>
-
         {/* Overview Tab View */}
         <TabsContent value="overview" className="space-y-4">
-          <StatCard
-            title="Total Revenue"
-            value="$45,231.89"
-            icon={DollarSign}
-          />
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+            <StatCard
+              title="Total Revenue"
+              value="$45,231.89"
+              icon={DollarSign}
+            />
             <StatCard title="Total Orders" value="12,234" icon={CreditCard} />
             <StatCard
               title="Unverified Orders"
@@ -79,61 +173,60 @@ export default function AdminDashboard() {
               icon={TriangleAlert}
             />
             <StatCard title="Pending Orders" value="543" icon={ClockArrowUp} />
+          </div>
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
             <StatCard title="Total Invoices" value="1,234" icon={FileText} />
             <StatCard title="Unpaid Invoices" value="1,234" icon={CircleX} />
             <StatCard title="Due Invoices" value="1,234" icon={ClockAlert} />
           </div>
-          {/* <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7"> */}
-          <div className="w-3/4 items-center justify-center">
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
             <Card className="col-span-3">
               <CardHeader>
                 <CardTitle>Recent Orders</CardTitle>
                 <CardDescription>
-                  {/* You made 265 sales this month. */}
+                  Your most recent order activity
                 </CardDescription>
               </CardHeader>
               <CardContent>
                 <RecentOrders data={adminRecentOrders} />
               </CardContent>
-              <CardFooter className="items-center justify-center">
-                <Link href="/admin/orders" className="mb-3" passHref>
-                  <Button className="w-full">View All Orders</Button>
+              <CardFooter className="flex justify-center">
+                <Link href="/admin/orders" passHref>
+                  <Button className="w-full sm:w-auto">View All Orders</Button>
                 </Link>
               </CardFooter>
             </Card>
           </div>
         </TabsContent>
-
         {/* Analytics Tab View */}
         <TabsContent value="analytics" className="space-y-4">
-          <div className="">
-            <Card className="col-span-4">
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            <Card className="col-span-3">
               <CardHeader>
-                <CardTitle>Overview</CardTitle>
+                <CardTitle>Order Overview</CardTitle>
               </CardHeader>
-              <CardContent className="pl-2">
-                <Overview data={adminOverViewData} />
+              <CardContent>
+                <Overview data={filteredOverviewData} />
               </CardContent>
             </Card>
-            <Card>
+            <Card className="col-span-3 md:col-span-2">
               <CardHeader>
                 <CardTitle>Revenue Growth</CardTitle>
               </CardHeader>
               <CardContent>
-                <RevenueGrowth data={adminRevenueGrowthData} />
+                <RevenueGrowth data={filteredRevenueData} />
               </CardContent>
             </Card>
-            <Card>
+            <Card className="col-span-3 md:col-span-1">
               <CardHeader>
                 <CardTitle>Product Performance</CardTitle>
               </CardHeader>
               <CardContent>
-                <ProductPerformance data={adminProductPerformanceData} />
+                <ProductPerformance data={filteredProductData} />
               </CardContent>
             </Card>
           </div>
         </TabsContent>
-
         {/* Report Tab View */}
         <TabsContent value="reports" className="space-y-4">
           <Card>
@@ -142,7 +235,7 @@ export default function AdminDashboard() {
               <CardDescription>View and download your reports</CardDescription>
             </CardHeader>
             <CardContent>
-              <ReportsTable />
+              <ReportsSection data={adminReportData} />
             </CardContent>
           </Card>
         </TabsContent>
