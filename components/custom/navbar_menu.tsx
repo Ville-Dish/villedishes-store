e89d84b14c4d813314528084fba159acc4a67f1e
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-11-21 13:36:32 -0700

    Fix lint issue with unnecessary call

--- components/custom/navbar_menu.tsx
+++ components/custom/navbar_menu.tsx
@@ -29,7 +29,7 @@ export const NavbarMenu = () => {
       window.addEventListener("resize", handleResize);
       return () => window.removeEventListener("resize", handleResize);
     }
-  }, []);
+  }, [handleResize]);
 
   const NavLink = ({
     href,
