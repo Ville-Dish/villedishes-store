1e90496471e0b1f7533f09aa5483b4b86d2f5966
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-11-22 17:26:50 -0700

    Refactor file to comply with lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import { Header } from "@/components/custom/header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
