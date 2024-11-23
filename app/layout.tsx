570d42c97cf1b519191343fc79ae928088364ec3
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-11-22 17:23:07 -0700

    Refactor file for conditional nav display

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header show />
         <div className="min-h-screen">{children}</div>
         <Toaster />
       </body>
