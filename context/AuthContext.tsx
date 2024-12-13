56b5d0b005caf5d242b7d175e6075c9a3d1bfcf0
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-12-13 13:19:31 -0700

    add file for authentication provider

---
+++ context/AuthContext.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { auth } from "@/config/firebase";
+import { onAuthStateChanged, signOut, User } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+
+// Define the shape of the AuthContext
+interface AuthContextType {
+  user: User | null;
+  logOut: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  return context;
+}
