123ef151c570731891bfa11d1a265cb4af4bec2c
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-12-13 14:16:58 -0700

    Refactor code to remove auth Helper code

--- lib/helper.ts
+++ lib/helper.ts
@@ -2,15 +2,3 @@ export const formattedCurrency = new Intl.NumberFormat("en-CA", {
   style: "currency",
   currency: "CAD",
 });
-import { adminAuth } from "@/config/firebaseAdmin";
-
-export async function verifyToken(req: Request) {
-  const token = req.headers.get("authorization")?.split(" ")[1];
-  if (!token) throw new Error("Unauthorized");
-
-  try {
-    await adminAuth.verifyIdToken(token);
-  } catch {
-    throw new Error("Unauthorized");
-  }
-}
