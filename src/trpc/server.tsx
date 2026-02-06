090649b1bdda9227c4ed99726636dd92361f4753
abiolaah <oluwapelumi.ajuwon@gmail.com>
2026-02-06 06:31:01 -0700

    initial trpc config

---
+++ src/trpc/server.tsx
@@ -0,0 +1,48 @@
+import "server-only";
+
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import {
+  createTRPCOptionsProxy,
+  TRPCQueryOptions,
+} from "@trpc/tanstack-react-query";
+import { cache } from "react";
+import { createTRPCContext } from "./init";
+import { makeQueryClient } from "./query-client";
+import { appRouter } from "./routers/_app";
+
+// IMPORTANT: Create a stable getter for the query client that
+//            will return the same client during the same request.
+
+export const getQueryClient = cache(makeQueryClient);
+
+export const caller = appRouter.createCaller(createTRPCContext);
+
+export const trpc = createTRPCOptionsProxy({
+  ctx: createTRPCContext,
+  router: appRouter,
+  queryClient: getQueryClient,
+});
+
+export const HydrateClient = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {children}
+    </HydrationBoundary>
+  );
+};
+
+export const prefetch = <T extends ReturnType<TRPCQueryOptions<any>>>(
+  queryOptions: T,
+) => {
+  const queryClient = getQueryClient();
+
+  const isInfiniteQuery = queryOptions.queryKey[1]?.type === "infinite";
+
+  if (isInfiniteQuery) {
+    void queryClient.prefetchInfiniteQuery(queryOptions as any);
+  } else {
+    void queryClient.prefetchQuery(queryOptions);
+  }
+};
